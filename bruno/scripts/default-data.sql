33ccb4aafc43b0dad0f62a37f3460a31954d5e5f
Bruno Krauss <info@brunokrauss.com>
2019-11-28 13:59:20 +0100

    scrupt for default data

---
+++ bruno/scripts/default-data.sql
@@ -0,0 +1,14 @@
+COPY "junction"
+FROM 'C:\Users\Bruno\Desktop\Coding\datenbanken-1\bruno\scripts\data\junctions.csv' DELIMITER ',' CSV HEADER;
+	
+COPY "line"
+FROM 'C:\Users\Bruno\Desktop\Coding\datenbanken-1\bruno\scripts\data\lines.csv' DELIMITER ',' CSV HEADER;
+	
+COPY "operator"
+FROM 'C:\Users\Bruno\Desktop\Coding\datenbanken-1\bruno\scripts\data\operators.csv' DELIMITER ',' CSV HEADER;
+	
+COPY "station"
+FROM 'C:\Users\Bruno\Desktop\Coding\datenbanken-1\bruno\scripts\data\stations.csv' DELIMITER ',' CSV HEADER;
+	
+COPY "zone"
+FROM 'C:\Users\Bruno\Desktop\Coding\datenbanken-1\bruno\scripts\data\zones.csv' DELIMITER ',' CSV HEADER;
