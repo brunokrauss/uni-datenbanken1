5eaaa647e72153569bbb2920be8a62e9510bbedf
paveluska <gitkraken@paveluska.de>
2019-11-28 14:52:07 +0100

    update pauls data imports

--- bruno/scripts/default-data.sql
+++ bruno/scripts/default-data.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 COPY "operator"
 FROM 'C:\Users\Bruno\Desktop\Coding\datenbanken-1\bruno\scripts\data\operators.csv' DELIMITER ',' CSV HEADER;
 
@@ -12,3 +13,19 @@ FROM 'C:\Users\Bruno\Desktop\Coding\datenbanken-1\bruno\scripts\data\junctions.c
 
 COPY "station"
 FROM 'C:\Users\Bruno\Desktop\Coding\datenbanken-1\bruno\scripts\data\stations.csv' DELIMITER ',' CSV HEADER;
+=======
+COPY "junction"
+FROM 'C:\Users\pmoeller\workspace\uni-datenbanken1\bruno\scripts\data\junctions.csv' DELIMITER ',' CSV HEADER;
+	
+COPY "line"
+FROM 'C:\Users\pmoeller\workspace\uni-datenbanken1\bruno\scripts\data\lines.csv' DELIMITER ',' CSV HEADER;
+	
+COPY "operator"
+FROM 'C:\Users\pmoeller\workspace\uni-datenbanken1\bruno\scripts\data\operators.csv' DELIMITER ',' CSV HEADER;
+	
+COPY "station"
+FROM 'C:\Users\pmoeller\workspace\uni-datenbanken1\bruno\scripts\data\stations.csv' DELIMITER ',' CSV HEADER;
+	
+COPY "zone"
+FROM 'C:\Users\pmoeller\workspace\uni-datenbanken1\bruno\scripts\data\zones.csv' DELIMITER ',' CSV HEADER;
+>>>>>>> Stashed changes
